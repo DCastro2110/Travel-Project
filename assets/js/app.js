82d5ff7ffb32e44eab4c8a1b00a6d655472d4f8d
Davi Castro dos Santos <59103658+d4v1z1n@users.noreply.github.com>
2022-03-18 22:10:18 -0300

    add dark mode and footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,15 @@
 (function() {
+    const checkbox = document.querySelector('.checkbox-dark-mode')
+
     document.addEventListener('click', (e) => {
         const el = e.target;
         const allOptions = document.querySelector('.slider-options-buy');
         const optionsBuy = document.querySelectorAll('.option-buy');
         const translateQuantify = ['0', 'calc(-100% / 3)', 'calc(-100% * 2 / 3)']
+
+        const darkModeDot = document.querySelector('.dark-mode-dot')
+
+        console.log(el)
 
         for (i in optionsBuy) {
             if (el === optionsBuy[i] || el.parentElement === optionsBuy[i]) {
@@ -12,7 +18,31 @@
                 el.classList.add('active');
             }
         }
+
+        if (el.classList.contains('dark-mode-div') || el === darkModeDot) {
+            checker()
+            darkModeBtn(darkModeDot)
+        }
     })
+   
+    function darkModeBtn(darkModeDot) {
+        if (!checkbox.checked) {
+            darkModeDot.style.transform = '';
+            darkModeDot.style.backgroundColor = '#fff'
+            return
+        }
+        darkModeDot.style.transform = 'translateX(20px)';
+        darkModeDot.style.backgroundColor = '#000052'
+    }
+
+    function checker() {
+        if (checkbox.checked) {
+            checkbox.checked = false
+            return
+        } 
+        checkbox.checked = true;
+    }
+
 }) ()
 
 
